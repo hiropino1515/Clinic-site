10c10345a903b6f4aaabd6bf64622fa6a358671c
hiropino1515 <hiropino1515@gmail.com>
2024-02-21 19:56:45 +0900

    footerのspまで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,47 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  // ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass('is-active');
+      $("body").removeClass('is-active');
+      $(".js-sp-nav").fadeOut(300);
+      $(".header").css('background', '#FFFFFF');
+    } else {
+      $(".js-hamburger").addClass('is-active');
+      $("body").addClass('is-active');
+      $(".js-sp-nav").fadeIn(300);
+      $(".header").css('background', '#0060A0');
+    }
+  });
 
+  // グローバルナビメニューのリンクをクリックしたらページを閉じる
+  $(function () {
+    $(".js-sp-nav ul li a").on("click", function () {
+      $(".js-hamburger").removeClass('is-active');
+      $(".js-sp-nav").fadeOut(300);
+      $(".header").css('background', '#FFFFFF');
+    });
+  });
+
+  // fv__swiper
+  var swiper = new Swiper(".js-fv-swiper", {
+    loop: true,
+    clickable: true,
+    effect: 'fade', // フェードのエフェクト
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 2500, // 2.5秒かけながら次の画像へ移動
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 
 });
